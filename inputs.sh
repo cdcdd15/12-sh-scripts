62f5d4eed38df3b4302482d1ccb00d12da346579
Cosmin Dinu <cdcdd15@gmail.com>
2021-06-16 22:09:02 +0300

    Added script that receives input values as script args.

---
+++ inputs.sh
@@ -0,0 +1,19 @@
+cat <<heredoc
+Caal this sctipt like this:
+./inputs 10 20 30
+heredoc
+
+echo "$1" "$2" "$3"
+
+#read inputs in array
+argArray=("$@")
+
+#read elements in array
+echo "${argArray[0]}" "${argArray[1]}" "${argArray[2]}"
+
+#array length
+echo "${#argArray[@]}"
+
+#print out all inputs
+len=${#argArray[@]}
+for (( i=0; i<$len; i++ )); do echo "${argArray[$i]}" ; done
