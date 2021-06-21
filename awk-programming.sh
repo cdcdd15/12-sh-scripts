6876b061161cb4d4ef7767d8d402122076061950
Cosmin Dinu <cdcdd15@gmail.com>
2021-06-21 09:23:07 +0300

    Added awk programming .sh script for calculating average temperature in C having as inputs temperatures expressed in C as well as expresses in F.

---
+++ awk-programming.sh
@@ -0,0 +1,12 @@
+echo 'Use awk tool for building as programming routine.'
+awk '
+	BEGIN{print "Run once."}
+	END{print "Run once (at the end)."}
+' temperature.csv
+echo 'Use awk too to aggregate values from input .csv file.'
+awk -F',' '
+	BEGIN{temp_sum=0; total_records=0; print "Begin calc of averate temparature."}
+	($2=="F"){temp_sum += ($1 - 32) / 1.8; total_records += 1;}
+	($2=="C"){temp_sum += $1; total_records += 1;}
+	END{print "Average temperature (in C): " temp_sum/total_records}
+' temperature.csv
